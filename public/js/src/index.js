2fed54d1b7f48cdeda3a8faaf6a62bf568040f77
Dan <git@dandart.co.uk>
2017-02-08 20:12:37 +0000

    socket skeleton

--- public/js/src/index.js
+++ public/js/src/index.js
@@ -1,4 +1,15 @@
 import $ from 'jquery';
 import 'bootstrap';
+import socketio from 'socket.io-client';
+
+const io = socketio();
+
+io.on('connection', socket => {
+    console.log(`A user connected`);
+
+    socket.on('disconnect', () => {
+        console.log(`Disconnected from server`);
+    })
+})
 
 console.log(`Hello World!`);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import socketio from 'socket.io';
+import http from 'http';
 
-const app = express();
+const app = express(),
+    server = http.Server(app),
+    io = socketio(server);
 
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 3000, () => console.log('Listening'));
+io.on('connection', socket => {
+    console.log(`A user connected`);
+});
+
+server.listen(process.env.PORT || 3000, () => console.log('Listening'));
